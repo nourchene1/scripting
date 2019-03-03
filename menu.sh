a04d34e1d96bbd8300f011e54566c24a584a2c4b
nourchene1 <saidi.nourchene@gmail.com>
2019-03-03 22:08:15 +0100

    main

---
+++ menu.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+menu()
+{
+echo "menu"
+echo "1-pour la commande lshw -lw"
+echo "2-pour la commande lscpu -lu"
+echo "3-pour la commande help -h"
+echo "4-save"
+echo "5-quitter"
+echo "VOTRE CHOIX"
+read choix
+
+}
+
+
+      
+
+
+
+
