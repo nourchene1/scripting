8485ffe84905456f0ff35699d298357e60e2f5e2
nourchene1 <saidi.nourchene@gmail.com>
2019-03-04 23:08:25 +0100

    menu

--- menu.sh
+++ menu.sh
@@ -1,20 +1,39 @@
-#!/bin/sh
-menu()
-{
-echo "menu"
-echo "1-pour la commande lshw -lw"
-echo "2-pour la commande lscpu -lu"
-echo "3-pour la commande help -h"
-echo "4-save"
-echo "5-quitter"
-echo "VOTRE CHOIX"
-read choix
+#!/bin/bash
 
-}
+source hardwarescript.sh
+source e1.sh
+source help.sh
+source e2.sh
+
+test=1
+yad --image='corebrum1.png' --title="Corebrum"
+while [[ $test == 1 ]]
+do
+ 
+
+ask=`zenity --list --title="Menu" --column="0" "Hardware" "Help"   --width=100 --height=300 --hide-header`
+if [ "$?" -eq 1 ]; then
+    #On quitte le script
+    exit	
+fi
+if [ "$ask" == "Hardware" ]; then
+Hardware="$(hardwareex)"
+    if ret=`zenity --info --title='Hardware' --text="$Hardware"`
+		then
+		echo "Hardware"						
+	fi
+fi
 
 
-      
+if [ "$ask" == "Help" ]; then  
+Hel="$(heplsss)"
+    if ret=`zenity --info --title='Help' --no-markup --no-wrap --text="$Hel"`
+		then
+		echo "Help"						
+	fi
+fi
 
 
 
-
+done
+exec ./menu.sh
